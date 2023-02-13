91fa3706a8c6b5c4d3e245fbeebdc7d796442ea6
BillyButcher95 <andrei.paun95@yahoo.com>
2023-02-13 19:47:48 +0200

    prepare form to submit new team

---
+++ index.js
@@ -0,0 +1,20 @@
+fetch("teams.json")
+  .then((r) => r.json())
+  .then((teams) => {
+    displayTeams(teams);
+  });
+
+function displayTeams(teams) {
+  const teamsHTML = teams.map(
+    (team) => `
+        <tr>
+            <td>${team.promotion}</td>
+            <td>${team.members}</td>
+            <td>${team.name}</td>
+            <td>${team.url}</td>
+            <td></td>
+        </tr>`
+  );
+
+  document.querySelector("#teams tbody").innerHTML = teamsHTML.join("");
+}
