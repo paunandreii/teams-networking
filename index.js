4af7aad3037922cb0d8657a2e657798352a2dfb1
BillyButcher95 <andrei.paun95@yahoo.com>
2023-02-23 19:40:17 +0200

    test

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-fetch("teams.json")
+fetch("http://localhost:3000/teams-json", {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
   .then((r) => r.json())
   .then((teams) => {
     displayTeams(teams);
@@ -8,13 +13,56 @@ function displayTeams(teams) {
   const teamsHTML = teams.map(
     (team) => `
         <tr>
-            <td>${team.promotion}</td>
-            <td>${team.members}</td>
-            <td>${team.name}</td>
-            <td>${team.url}</td>
-            <td></td>
+          <td>${team.promotion}</td>
+          <td>${team.members}</td>
+          <td>${team.name}</td>
+          <td>${team.url}</td>
+          <td>
+              <a>✖</a>
+          </td>
         </tr>`
   );
 
   document.querySelector("#teams tbody").innerHTML = teamsHTML.join("");
 }
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  fetch("http://localhost:3000/teams-json/create", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      promotion: document.getElementById("promotion").value,
+      members: document.getElementById("members").value,
+      name: document.getElementById("name").value,
+      url: document.getElementById("url").value,
+    }),
+  })
+    .then((r) => r.json())
+    .then((status) => {
+      console.warn("status", status.success, status.id);
+      if (status.success) {
+        window.location.reload();
+      }
+    });
+}
+
+function removeTeamRequest() {
+  fetch("http://localhost:3000/teams-json/delete", {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ id: "fedcba1610309909431" }),
+  });
+}
+
+function initEvents() {
+  const form = document.getElementById("editForm");
+  form.addEventListener("submit", onSubmit);
+}
+
+initEvents();
